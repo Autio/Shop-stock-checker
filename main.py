aba0ae7ac6cd716d46a0f163c9ae0b36a0600e3a
Autio <petri.mikael.autio@gmail.com>
2021-06-09 00:08:43 +0300

    Emails

--- main.py
+++ main.py
@@ -2,50 +2,74 @@
 import os
 import smtplib
 import urllib.request
+from datetime import datetime
 from email.message import EmailMessage
 from bs4 import BeautifulSoup
 
+log = ""
 
 def check_availability():
-    target_page = 'https://www.prisma.fi/fi/prisma/prisma-gaming-playstation-5'
-    target_phrase = 'Playstation 5-pelikonsolit tilapäisesti loppu'
-    page = urllib.request.urlopen(target_page)
-    soup = BeautifulSoup(page)
+    global log
+    try:
+        target_page = 'https://www.prisma.fi/fi/prisma/prisma-gaming-playstation-5'
+        target_phrase = 'Playstation 5-pelikonsolit tilapäisesti loppu'
+        page = urllib.request.urlopen(target_page)
+        soup = BeautifulSoup(page)
 
-    if target_phrase in soup.text:
-        return False
-    return True
+        if target_phrase in soup.text:
+            return False
+        return True
+    except:
+        log += "Error parsing website "
     
 
 def main():
-    available = True
-    #available = check_availability()
-    from_address = "petri.mikael.autio@gmail.com"
-    to_address = "anssi.ekmark@gmail.com"
-
-    if(available):
+    global log
+    
+    available = check_availability()
+    logfile = open('log.txt', 'r+')
+    successmessage = "PS5 looks to be available! "
+    
+    # Check if the PS5 has already been spotted once
+    # Note, the log needs resetting if you want this process to continue
+    if successmessage in logfile.read():
+        print("PS5 already found")
+        return
+    
+    if(available):    
+        log += successmessage
+        
+        try:
+            with open('credentials') as file:
+                lines = file.readlines()
+                username = lines[0].rstrip('\n')
+                password = lines[1].rstrip('\n')
+                from_address = lines[2].rstrip('\n')
+                to_address = lines[3].rstrip('\n')
+        except: 
+            log += "Error with credentials file "      
+        
         msg = EmailMessage()
         msg['Subject'] = "PS5 hälytys"
         msg['From'] = from_address
-        msg['To'] = from_address
-        msg.set_content("No nyt sitä pleikkaa o.\n Kato vaikka: https://www.prisma.fi/fi/prisma/prisma-gaming-playstation-5")
-
-        with open('credentials') as file:
-            lines = file.readlines()
-            username = lines[0].rstrip('\n')
-            password = lines[1].rstrip('\n')
-        x = 5
-        
-        #try: 
-        server = smtplib.SMTP('mail.upbeam.net', 23)
-        server.login(username, password)
-        server.ehlo()
-        server.starttls()
-        
-        server.send_message(msg)
-        server.quit()
-    #except: 
-        print('Sending the email failed.')
+        msg['To'] = to_address
+        msg.set_content("No nyt sitä pleikkaa o.\nKato vaikka: https://www.prisma.fi/fi/prisma/prisma-gaming-playstation-5")
 
 
+        try: 
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.ehlo()
+            server.starttls()
+            server.login(username, password)
+
+            server.send_message(msg)
+            server.quit()
+
+            log += "Message sent! "
+        except: 
+            log += "Error sending email "
+    else:
+        log += "No PS5 seems to be available "
+    logfile.write(str(datetime.now()) + " " + log + "\n")
+    logfile.close()
 main()
